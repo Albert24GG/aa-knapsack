ac2c7f35ce1fb89ddad0e1b19061cc45daee84c1
Albert24GG <guiman.albert09@gmail.com>
2025-01-08 13:10:20 +0200

    feat: Update checker script and tests

--- checker/check.sh
+++ checker/check.sh
@@ -1,69 +1,35 @@
 #!/bin/bash
 
-# Usage: ./check.sh [--clear] [--save] [--help]
-# --clear: Clear the output directory
-# --save: Save the output of the checker
-# --help: Display this message
-
-if [ "$1" == "--help" ]; then
-    echo "Usage: ./check.sh [--clear] [--save] [--help]"
-    echo "--clear: Clear the output directory"
-    echo "--save: Save the output of the checker"
-    echo "--help: Display this message"
-    exit 0
-fi
-
-# Clear the output directory
-
-if [ "$1" == "--clear" ]; then
-    rm -rf ../output
-    exit 0
-fi
-
-TEST_DIR=./tests
-ANSWERS_DIR=./answers
-OUTPUT_DIR=./output
-
-xmake f -m release
-xmake clean
-xmake
-
-cargo build -r 2>/dev/null
-
-rm debug.log
-
-# Precalculate the answers
-if [ ! -d $ANSWERS_DIR ]; then
-    mkdir -p $ANSWERS_DIR
-    find "$TEST_DIR" -name "*.kp" -exec sh -c 'xmake r knapsack_dp <"$1" >$ANSWERS_DIR/$(basename "$0" .kp).ans' {} \;
-fi
-
-# Save the output if specified
-if [ "$1" == "--save" ]; then
-    mkdir -p $OUTPUT_DIR/{bkt,dp,fptas}
-fi
+# @option -o --output-dir=./output The output directory
+# @option -t --test-dir=./tests The directory containing the tests
+# @option -a --answers-dir=./answers The directory where the answers are stored
+# @option -g --granularity=1  The granularity to be used for fptas
+# @flag -s --save Save the output of the tests
 
 # Run the tests
 # $1: test file
 # $2: algorithm
 run_test() {
-    ANSWER=$(cat $ANSWERS_DIR/$(basename "$1" .kp).ans)
-    cargo run -r -- --input-file "$1" run "$2" 2>>debug.log
+    ANSWER=$(cat "$argc_answers_dir"/$(basename "$1" .kp).ans)
+    cargo run -r -- --input-file "$1" --granularity "$argc_granularity" run "$2" 2>>debug.log
     # Output saved in out.json
-    if [ "$3" == "--save" ]; then
-        cp out.json $OUTPUT_DIR/"$2"/$(basename "$1" .kp).json
+    if [ "$argc_save" ]; then
+        cp out.json "$argc_output_dir"/"$2"/$(basename "$1" .kp).json
     fi
     VALUE=$(jq -r '.total_value' out.json)
     ITEMS=$(jq -r '.items | @sh' out.json)
+    rm out.json
 
     if [ "$2" == "fptas" ]; then
         # Check how close the value is to the answer
         PERCENTAGE=$(echo "scale=5; 100 * $VALUE / $ANSWER" | bc)
-        echo "Fptas: VALUE is $PERCENTAGE% of ANSWER"
+        echo "Fptas for granularity $argc_granularity: VALUE is $PERCENTAGE% of ANSWER"
         return 0
     fi
 
     if [ "$ANSWER" != "$VALUE" ]; then
+        echo "Expected: $ANSWER"
+        echo "Got: $VALUE"
         echo "Value mismatch"
         return 1
     fi
@@ -72,52 +38,129 @@ run_test() {
     return $?
 }
 
-echo "Running small tests (dp and bkt)"
-echo
+run_special_tests() {
+    echo
+    echo "Running special tests (fptas only)"
+    echo
 
-for test in "$TEST_DIR"/small/*.kp; do
-    echo "Running $(basename "$test")"
-    run_test "$test" "bkt" "$1"
-    if [ $? -eq 1 ]; then
-        echo "bkt failed"
+    for test in "$argc_test_dir"/special/*.kp; do
+        echo "Running $(basename "$test")"
+        run_test "$test" "fptas"
+        if [ $? -eq 1 ]; then
+            echo "failed"
+            continue
+        fi
+        echo "Test passed"
+    done
+    exit 0
+}
+
+run_small_tests() {
+    echo
+    echo "Running small tests (dp and bkt)"
+    echo
+    for test in "$argc_test_dir"/small/*.kp; do
+        echo "Running $(basename "$test")"
+        run_test "$test" "bkt"
+        if [ $? -eq 1 ]; then
+            echo "bkt failed"
+        fi
+        run_test "$test" "dp"
+        if [ $? -eq 1 ]; then
+            echo "dp failed"
+            continue
+        fi
+        run_test "$test" "fptas"
+        echo "Test passed"
+    done
+}
+
+run_mid_tests() {
+    echo
+    echo "Running mid tests (dp only)"
+    echo
+
+    for test in "$argc_test_dir"/mid/*.kp; do
+        echo "Running $(basename "$test")"
+        run_test "$test" "dp"
+        if [ $? -eq 1 ]; then
+            echo "dp failed"
+            continue
+        fi
+        run_test "$test" "fptas"
+        echo "Test passed"
+    done
+}
+
+run_large_tests() {
+    echo
+    echo "Running large tests (dp only)"
+    echo
+
+    for test in "$argc_test_dir"/large/*.kp; do
+        echo "Running $(basename "$test")"
+        run_test "$test" "dp"
+        if [ $? -eq 1 ]; then
+            echo "dp failed"
+            continue
+        fi
+        run_test "$test" "fptas"
+        echo "Test passed"
+    done
+}
+
+init() {
+    xmake f -m release
+    xmake clean
+    xmake
+
+    cargo build -r 2>/dev/null
+
+    # Save the output if specified
+    if [ "$argc_save" ]; then
+        mkdir -p $argc_output_dir/{bkt,dp,fptas}
     fi
-    run_test "$test" "dp" "$1"
+}
+
+# @cmd Run a single test
+# @arg test! <PATH>   The path to the test file
+# @arg method! <METHOD>     The method/algorithm to be used
+run_one_test() {
+    # Recalculate the answer
+    init
+    # Precalculate the answer
+    xmake r knapsack_dp <"$argc_test" >$argc_answers_dir/$(basename "$argc_test" .kp).ans
+
+    run_test "$argc_test" "$argc_method"
     if [ $? -eq 1 ]; then
-        echo "dp failed"
-        continue
+        echo "Test $argc_test failed"
+    else
+        echo "Test $argc_test passed"
     fi
-    run_test "$test" "fptas" "$1"
-    echo "Test passed"
-done
+    exit 0
+}
 
-echo
-echo "Running mid tests (dp only)"
-echo
+# @cmd Run all tests
+# @flag --special Include special tests
+run_all_tests() {
+    init
 
-for test in "$TEST_DIR"/mid/*.kp; do
-    echo "Running $(basename "$test")"
-    run_test "$test" "dp" "$1"
-    if [ $? -eq 1 ]; then
-        echo "dp failed"
-        continue
+    # Precalculate the answers
+    if [ ! -d $argc_answers_dir ]; then
+        echo "Precalculating answers"
+        mkdir -p $argc_answers_dir
+        find "$argc_test_dir" -name "*.kp" -exec bash -c 'xmake r knapsack_dp <$0 >$1/$(basename "$0" .kp).ans' {} $argc_answers_dir \;
     fi
-    run_test "$test" "fptas" "$1"
-    echo "Test passed"
-done
 
-echo
-echo "Running large tests (dp only)"
-echo
+    run_small_tests
+    run_mid_tests
+    run_large_tests
 
-for test in "$TEST_DIR"/large/*.kp; do
-    echo "Running $(basename "$test")"
-    run_test "$test" "dp" "$1"
-    if [ $? -eq 1 ]; then
-        echo "dp failed"
-        continue
+    if [ "$argc_special" ]; then
+        run_special_tests
     fi
-    run_test "$test" "fptas" "$1"
-    echo "Test passed"
-done
 
-rm out.json
+    exit 0
+}
+
+eval "$(argc --argc-eval "$0" "$@")"
