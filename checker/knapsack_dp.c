d1482b567d8a9ec8f99d01d93f16f669dc5f9ded
Albert24GG <guiman.albert09@gmail.com>
2025-01-12 13:47:27 +0200

    fix: Use calloc instead of stack allocation

--- checker/knapsack_dp.c
+++ checker/knapsack_dp.c
@@ -1,37 +1,34 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int knap_sack(uint64_t W, uint32_t wt[], uint32_t val[], uint32_t n) {
-    uint32_t dp[W + 1];
+  uint32_t *dp = calloc(W + 1, sizeof(uint32_t));
 
-    for (size_t i = 0; i <= W; i++) {
-        dp[i] = 0;
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = W; j >= wt[i]; j--) {
+      dp[j] = dp[j] > dp[j - wt[i]] + val[i] ? dp[j] : dp[j - wt[i]] + val[i];
     }
+  }
 
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = W; j >= wt[i]; j--) {
-            dp[j] = dp[j] > dp[j - wt[i]] + val[i] ? dp[j] : dp[j - wt[i]] + val[i];
-        }
-    }
-
-    return dp[W];
+  return dp[W];
 }
 
 int main() {
-    uint32_t n;
-    uint64_t w;
-    scanf("%u %lu", &n, &w);
+  uint32_t n;
+  uint64_t w;
+  scanf("%u %lu", &n, &w);
 
-    if (n == 0 || w == 0) {
-        printf("0\n");
-        return 0;
-    }
-
-    uint32_t weights[n], values[n];
-    for (size_t i = 0; i < n; i++) {
-        scanf("%u %u", &values[i], &weights[i]);
-    }
-
-    printf("%d\n", knap_sack(w, weights, values, n));
+  if (n == 0 || w == 0) {
+    printf("0\n");
     return 0;
+  }
+
+  uint32_t weights[n], values[n];
+  for (size_t i = 0; i < n; i++) {
+    scanf("%u %u", &values[i], &weights[i]);
+  }
+
+  printf("%d\n", knap_sack(w, weights, values, n));
+  return 0;
 }
